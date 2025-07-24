97dc1c8dddf4219b5f5f1fe851ee4897c982948a
Juna <itujun7@gmail.com>
2025-07-24 23:17:06 +0700

    embed file ke []byte

--- embed_test.go
+++ embed_test.go
@@ -3,6 +3,8 @@ package golang_embed
 import (
 	_ "embed"
 	"fmt"
+	"io/fs"
+	"os"
 	"testing"
 )
 
@@ -17,4 +19,22 @@ var version2 string
 func TestEmbedFileToString(t *testing.T){
 	fmt.Println(version);
 	fmt.Println(version2);
+}
+
+//go:embed favicon.ico 
+var favicon []byte
+
+//go:embed logo/logo.png 
+var logo []byte
+
+func TestEmbedFileToByteArray(t *testing.T){
+	err := os.WriteFile("favicon_new.ico", favicon, fs.ModePerm)
+	if err != nil {
+		panic(err)
+	}
+
+	err = os.WriteFile("logo/logo_new.png", logo, fs.ModePerm)
+	if err != nil {
+		panic(err)
+	}
 }
